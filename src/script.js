f1de200d22765f8c87431d12f15ce1a25bf814ba
Practice <Zuhairann@gmail.com>
2025-01-22 16:19:56 +0500

    Add Karachi Fares & Set Height

--- src/script.js
+++ src/script.js
@@ -2,21 +2,39 @@ jQuery('.Destination').chosen();
 
 const Shalimar =document.querySelector('.shalimar');
 const Karakoram =document.querySelector('.kara')
+const Millat =document.querySelector('.Millat')
+const Pakistan =document.querySelector('.Pakistan')
+const Rehman =document.querySelector('.Rehman')
 
 
 // Select all elements with the class "station"
 let station = document.querySelectorAll('.station');
+// Shalimar Fares
 let acbsh = document.querySelectorAll('.acbsh');
 let apsh = document.querySelectorAll('.apsh');
 let actsh = document.querySelectorAll('.actsh');
 let ebsh = document.querySelectorAll('.ebsh');
 let essh = document.querySelectorAll('.essh');
-
+// Karakoram Fares
 let acbk = document.querySelectorAll('.acbk');
 let apk = document.querySelectorAll('.apk');
 let actk = document.querySelectorAll('.actk');
 let ebk = document.querySelectorAll('.ebk');
 let esk = document.querySelectorAll('.esk');
+// Millat Fares
+let abm = document.querySelectorAll('.abm') 
+let ebm = document.querySelectorAll('.ebm') 
+let esm = document.querySelectorAll('.esm')
+// Pakistan Fares
+let asp = document.querySelectorAll('.asp') 
+let ebp = document.querySelectorAll('.ebp') 
+let esp = document.querySelectorAll('.esp')
+// Rehman baba Fares
+let abr = document.querySelectorAll('.abr') 
+let asr = document.querySelectorAll('.asr') 
+let ebr = document.querySelectorAll('.ebr') 
+let esr = document.querySelectorAll('.esr')
+
 
 
 // Store the original text in a data attribute
@@ -92,6 +110,82 @@ function updateName(selectedLi) {
       ele.textContent = "1050";
     })
   }
+
+  else if (selectedLi.innerText === "KARACHI CANTT") {
+    // Shalimar
+    Shalimar.classList.remove ('hidden');
+    acbsh.forEach((ele) => {
+      ele.textContent = "6700";
+    })
+    apsh.forEach((ele) => {
+      ele.textContent = "6450";
+    })
+    actsh.forEach((ele) => {
+      ele.textContent = "5050";
+    })
+    ebsh.forEach((ele) => {
+      ele.textContent = "3000";
+    })
+    essh.forEach((ele) => {
+      ele.textContent = "2900";
+    })
+
+    //  Karakoram
+    Karakoram.classList.remove ('hidden');
+    acbk.forEach((ele) => {
+      ele.textContent = "7250";
+    })
+    actk.forEach((ele) => {
+      ele.textContent = "5600";
+    })
+    ebk.forEach((ele) => {
+      ele.textContent = "3700";
+    })
+    esk.forEach((ele) => {
+      ele.textContent = "3600";
+    })
+
+    //  MILLAT
+    Millat.classList.remove ('hidden');
+    abm.forEach((ele) => {
+      ele.textContent = "6700";
+    })
+    ebm.forEach((ele) => {
+      ele.textContent = "3000";
+    })
+    esm.forEach((ele) => {
+      ele.textContent = "2900";
+    })
+
+    //  Pakistan
+    Pakistan.classList.remove ('hidden');
+    
+    asp.forEach((ele) => {
+      ele.textContent = "4800";
+    })
+    ebp.forEach((ele) => {
+      ele.textContent = "2850";
+    })
+    esp.forEach((ele) => {
+      ele.textContent = "2750";
+    })
+
+    //  Rehman Baba
+    Rehman.classList.remove ('hidden');
+    abr.forEach((ele) => {
+      ele.textContent = "6350";
+    })
+    asr.forEach((ele) => {
+      ele.textContent = "4800";
+    })
+    ebr.forEach((ele) => {
+      ele.textContent = "2850";
+    })
+    esr.forEach((ele) => {
+      ele.textContent = "2750";
+    })
+  }
+  
 
   // Ending of cities
   // Replace "Lahore" with the selected country in station elements
